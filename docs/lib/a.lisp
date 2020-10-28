651a1f61c183b0714dec453409abe62752f66521
Tim Menzies <timm@ieee.org>
2020-10-28 00:58:45 -0400

    save

--- docs/lib/a.lisp
+++
@@ -1,9 +0,0 @@
-(or (fboundp 'env)  (defun env  (x &optional d) (or #+CLISP (ext:getenv x) #+SBCL (sb-unix::posix-getenv x) d)))
-(or (fboundp 'want) (defun uses (f)             (load (format nil "~a/~a" (env "Gator" ".") f))))
-; # asdaasdad  asdas
-; 
-
-(uses "cols/num")
-(defmethod aa(x) (+ x 1))
-(print (make-num))
-
--- docs/lib/a.md
+++ docs/lib/a.md
@@ -1,7 +1,7 @@
 <img align=right src="https://raw.githubusercontent.com/timm/gator/main/docs/img/lisplogo_256.png">
 
-# asdaasdad  asdas
-  
+# asdaasdad  asdas  
+    
 ```lisp
 (uses "cols/num")
 (defmethod aa(x) (+ x 1))
