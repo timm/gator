203e4fd1cdfd77eda97ded9eae1433a60b06438b
Tim Menzies <timm@ieee.org>
2020-10-29 19:25:41 -0400

    save

--- src/lib/sandbox.lisp
+++ src/lib/sandbox.lisp
@@ -1,17 +1,7 @@
 "##  My simple documenter
 
-Convert lisp code to markdown
-Strings are printed verbatim.
-defuns get their doco string pulled.
-"
-
-(defmacro while (test &body body) 
-  "While loop"
-   `(do () ((not ,test)) ,@body))
-
-(defun line1(in) 
-  "Pull first line from string"
-  (with-input-from-string (s in) (read-line s nil)))
+Convert lisp code to markdown Strings are printed verbatim.  defuns
+get their doco string pulled.  "
 
 (defvar *body* "## ~(~a~)
 
@@ -24,19 +14,23 @@ defuns get their doco string pulled.
 ```
 </details></ul>~%")
 
-(defun reads (&aux x)
-  "Low -level worker"
-  (format t "~a" (with-output-to-string(main)
-    (format t "~a" (with-output-to-string (top)
-      (while (setf x (read-preserving-whitespace t nil))
-        (when (stringp x) 
-          (format main "~%~a~%" x))
-        (when (listp x)
-          (let ((pre (first  x))
-                (f   (second x))
-                (a   (fourth x)))
-            (when (member pre `(defun defmacro defstruct defmethod))
-              (format top "- [~(~a~)](#~(~a~)) : ~a~%" f f (line1 a))
-              (format main *body* f a x))))))))))
+(defmacro while (test &body body) 
+  "While loop"
+   `(do () ((not ,test)) ,@body))
 
-(reads)
+(defun line1(in) 
+  "Pull first line from string"
+  (with-input-from-string (s in) (read-line s nil)))
+
+(format t "~a" (with-output-to-string(main)
+  (format t "~a" (with-output-to-string (top)
+    (while (setf x (read-preserving-whitespace t nil))
+      (when (stringp x) 
+        (format main "~%~a~%" x))
+      (when (listp x)
+        (let ((pre (first  x))
+              (f   (second x))
+              (a   (fourth x)))
+          (when (member pre `(defun defmacro defstruct defmethod))
+            (format top "- [~(~a~)](#~(~a~)) : ~a~%" f f (line1 a))
+            (format main *body* f a x)))))))))
