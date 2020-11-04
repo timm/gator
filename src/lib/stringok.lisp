ba97ccea7c0bc7a106e44672a8141f9ad2ef2540
Tim Menzies <timm@ieee.org>
2020-11-03 19:54:03 -0500

    save

--- src/lib/stringok.lisp
+++ src/lib/stringok.lisp
@@ -1,10 +1,12 @@
 (load "../lib/string")
 
 ;#+a(let ((s "I like, traffic lights.  I like traffic lights.  Especially when,     they are green")) (dolist (line (lines s)) (format t "~&~a~%" line)))
-         
-(let ((n 0)) 
-  (with-csv (lst "../../data/auto93000.csv") 
-    (incf n (length lst)))
-  (print n))
+(declaim (optimize speed)) 
+ (require :sb-sprof)
 
-  ;        (format t "~&~a~%" line)) 
+(sb-sprof:with-profiling 
+  (:max-samples 1 :report :flat :loop nil)
+  (let ((n 0)) 
+    (with-csv (lst "../../data/auto93000.csv") 
+      (incf n (length lst)))
+    (print n)))
