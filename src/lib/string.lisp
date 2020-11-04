2b2c0add98174686944df7da69264a82e86c64fd
Tim Menzies <timm@ieee.org>
2020-11-03 19:37:55 -0500

    save

--- src/lib/string.lisp
+++ src/lib/string.lisp
@@ -22,18 +22,21 @@
   Skip columns starting with `?`. 
   Coerce strings to numbers (if needed)."
   (with-open-file (str file) 
-    (labels
-      ((wanted (xs fs &aux (f  (pop fs)) (x (pop xs)))
-          (if f 
-            (cons (funcall f x) (and xs (wanted xs fs))) 
-            (and xs (wanted xs fs))))
-       (cells (str &optional (lo 0) (hi (position #\, str :start (1+ lo))))
-          (cons (string-trim '(#\Space #\Tab #\Newline) (subseq str lo hi))
-                (and hi (cells str (1+ hi)))))
-       (prep1 (x) (unless (eql #\? (char x 0)) 
-                     (if (member (char x 0) '(#\< #\> #\$)) 
-                       #'read-from-string #'identity))))
-      (let (prep width) ; memory across all lines
+    (let ((first t) prep width) ; memory across all lines
+      (labels
+        ((fromString (x) (if first
+                           x 
+                           (if (equal x "?") x (read-from-string x))))
+         (wanted (xs fs &aux (f  (pop fs)) (x (pop xs)))
+                 (if f 
+                   (cons (funcall f x) (and xs (wanted xs fs))) 
+                   (and xs (wanted xs fs))))
+         (cells (str &optional (lo 0) (hi (position #\, str :start (1+ lo))))
+                (cons (string-trim '(#\Space #\Tab #\Newline) (subseq str lo hi))
+                      (and hi (cells str (1+ hi)))))
+         (prep1 (x) (unless (eql #\? (char x 0)) 
+                      (if (member (char x 0) '(#\< #\> #\$)) 
+                        #'fromString #'identity))))
         (loop (let (vals tmp) ; memory of this time
                 (if (setf tmp (read-line str nil))
                   (when (> (length tmp) 0)
@@ -42,5 +45,6 @@
                           vals  (wanted tmp prep)
                           width (or width (length vals)))
                     (assert (eql width (length vals)))
-                    (funcall fun vals))
+                    (funcall fun vals)
+                    (setf first nil))
                   (return-from csv))))))))
--- src/lib/stringok.lisp
+++ src/lib/stringok.lisp
@@ -2,7 +2,9 @@
 
 ;#+a(let ((s "I like, traffic lights.  I like traffic lights.  Especially when,     they are green")) (dolist (line (lines s)) (format t "~&~a~%" line)))
          
-(with-csv (lst "../../data/weather.csv")
-   (print lst))
+(let ((n 0)) 
+  (with-csv (lst "../../data/auto93000.csv") 
+    (incf n (length lst)))
+  (print n))
 
   ;        (format t "~&~a~%" line)) 
