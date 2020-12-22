01951107e93c527783fce639a5be3868ac1402a3
Tim Menzies <timm@ieee.org>
2020-12-22 10:57:46 -0500

    save

--- src/lib/doc.lisp
+++ src/lib/doc.lisp
@@ -1,25 +1,26 @@
-"##  My simple documenter
-
-Convert lisp code to markdown.  Strings are printed verbatim.
-defuns, defmacros, defmethods, defclass, defstructs get their doco
-string pulled, then topped with a &lt;h3> heading. And a table of
-contents is added to the top of page.
-
-Note: 30 lines of code! :wink:
-
-Usage: 
-
-    cat file.lisp | lisp doc.lisp
-
-The system is controlled by two variables:
-
-- The `want` variable.  An entry (e.g.) `(defun . 3)` 
-  says that we will display lists starting with `defun` 
-  if it has a doc string as element `3` 
-  (which means the fourth thing in the list).
-- The `fmt` variable which controls how we render 
-  something's name, synopsis, and doc string.
-"
+ 
+; My simple documenter
+;
+; Convert lisp code to markdown.  Strings are printed verbatim.
+; defuns, defmacros, defmethods, defclass, defstructs get their doco
+; string pulled, then topped with a &lt;h3> heading. And a table of
+; contents is added to the top of page.
+;
+; Note: 30 lines of code! :wink:
+;
+; Usage: 
+;
+;     cat file.lisp | lisp doc.lisp
+;
+; The system is controlled by two variables:
+;
+; - The `want` variable.  An entry (e.g.) `(defun . 3)` 
+;   says that we will display lists starting with `defun` 
+;   if it has a doc string as element `3` 
+;   (which means the fourth thing in the list).
+; - The `fmt` variable which controls how we render 
+;   something's name, synopsis, and doc string.
+;
 
 (defvar *fmt*
 "~%### ~(~a~)
